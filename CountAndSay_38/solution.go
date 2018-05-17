6ed70cf4473f149b3d2f53628ed307b03d498fc4
Vasiliy Upornikov <vupornikov@avito.ru>
2018-05-18 01:30:02 +0300

    Optimized "38. Count and Say"

--- CountAndSay_38/solution.go
+++ CountAndSay_38/solution.go
@@ -1,24 +1,22 @@
 package CountAndSay_38
 
-import "strconv"
-
 func countAndSay(n int) string {
 	if n < 1 {
 		return ""
 	}
 
-	say := "1"
+	say := []byte("1")
 	for i := 0; i < n-1; i++ {
-		cnt := 0
-		nextSay := ""
+		var cnt byte
+		var nextSay []byte
 		for j := 0; j < len(say); j++ {
 			cnt++
 			if j == len(say)-1 || say[j] != say[j+1] {
-				nextSay += strconv.Itoa(cnt) + string(say[j])
+				nextSay = append(nextSay, 48 + cnt, say[j]) // 48 - the code point of "0"
 				cnt = 0
 			}
 		}
 		say = nextSay
 	}
-	return say
+	return string(say)
 }
